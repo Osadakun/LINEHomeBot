cdc06c0757282a158c68eac1ca43f920d3210be0
小野紀輝 <onotoshiki@onoosamuakiranoMacBook-Pro.local>
2021-05-13 01:26:38 +0900

    fix

--- setting.py
+++ setting.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 
 ENGINE = create_engine(
-    "postgres://ylcywgvpxrfldl:7f634f2f7881359957c4483720f765f28ef2e78677c230352f4868d05d156bc9@ec2-3-234-22-132.compute-1.amazonaws.com:5432/db7j7l1274gbnb",
+    "postgresql://ylcywgvpxrfldl:7f634f2f7881359957c4483720f765f28ef2e78677c230352f4868d05d156bc9@ec2-3-234-22-132.compute-1.amazonaws.com:5432/db7j7l1274gbnb",
     encoding = "utf-8",
     echo=True # Trueだと実行のたびにSQLが出力される
 )
