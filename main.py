a969cb25f451ec884cda7da423b86df941b2875c
小野紀輝 <onotoshiki@onoosamuakiranoMacBook-Pro.local>
2021-05-13 22:29:29 +0900

    fix

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from setting import session
 # Userモデルの取得
 from user import *
 from sqlalchemy import *
-
 # DBにレコードの追加
 user = User()
 user.name = '太郎'
@@ -14,4 +13,3 @@ session.commit()
 users = session.query(User).all()
 for user in users:
     print(user.name)
-
