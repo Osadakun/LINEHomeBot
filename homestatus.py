f89b02b22fee59aa17e4aaa3f9d69ab744383582
小野紀輝 <b1915053@planet.kanazawa-it.ac.jp>
2021-05-28 15:46:05 +0900

    db-test

---
+++ homestatus.py
@@ -0,0 +1,51 @@
+import config
+import psycopg2
+
+from flask import Flask, request, abort
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, MessageAction, TemplateSendMessage,
+    ButtonsTemplate,TextSendMessage)
+
+app = Flask(__name__)
+
+line_bot_api = LineBotApi(config.ACCESS_TOKEN)
+handler = WebhookHandler(config.CHANNEL_SECRET)
+
+@app.route("/")
+def hello_world():
+    return "HelloWorld!"
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+conn = psycopg2.connect(dsn=config.PG_URL)
+cur = conn.cursor()
+cur.execute('SELECT * FROM Family_Member')
+cur.commit()
+results = cur.fetchall()
+for r in results:
+  line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=r))
+
+
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
